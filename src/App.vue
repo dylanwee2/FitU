d41deeee8a1e7347b2e1d4d8201fabaea13aa168
defton290 <70007384+defton290@users.noreply.github.com>
2025-10-04 12:54:15 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <nav>
+      <!-- Static navigation (always here) -->
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <!-- Dynamic content (changes based on URL) -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
