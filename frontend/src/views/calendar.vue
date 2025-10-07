5d39ecf454e973a79cd8d33fb79984a22ad53ae4
Jun Wei Png <junweipng@gmail.com>
2025-10-07 12:47:53 +0800

    Create calendar.vue

---
+++ frontend/src/views/calendar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="calendarRef"></div>
+</template>
+
+<script setup>
+import { ref, onMounted, nextTick } from 'vue'
+import { Calendar } from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+const calendarRef = ref(null)
+
+onMounted(async () => {
+  await nextTick()
+  if (calendarRef.value) {
+    const calendar = new Calendar(calendarRef.value, {
+      plugins: [dayGridPlugin],
+      initialView: 'dayGridMonth'
+    })
+    calendar.render()
+  } else {
+    console.error('calendarRef is null')
+  }
+})
+</script>
